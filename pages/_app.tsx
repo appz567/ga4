7e6866873413f10039187fce99a0dd503b4386bd
PETROPOULOS EVANGELOS <e.petropoulos@pmmretail.com>
2023-02-02 14:12:39 +0200

    initial

---
+++ pages/_app.tsx
@@ -0,0 +1,16 @@
+import '@/styles/globals.css'
+import type { AppProps } from 'next/app'
+
+import { useEffect } from 'react';
+
+import TagManager from 'react-gtm-module';
+
+
+export default function App({ Component, pageProps }: AppProps) {
+
+  useEffect(() => {
+    TagManager.initialize({ gtmId: 'GTM-52G59DQ'})
+  }, [])
+
+  return <Component {...pageProps} />
+}
